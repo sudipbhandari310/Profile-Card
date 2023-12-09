85c7b2a82fdf39009117e3c0fbb3bd73b7d1f034
Sudip <sudipbhandari310@gmail.com>
2023-12-09 15:13:18 +0545

    letsgo

--- src/index.js
+++ src/index.js
@@ -1,13 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Card() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+      </div>
+      <SkillList />
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="pp.jpg" alt="sdf"></img>;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Sudip Bhandari</h1>
+      <p>
+        I am currently a student at Texas International College. I am a computer
+        science student.And right now i am learning React js and plant to
+        develop my skill on MERN stack.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="React" emoji="😠" />
+      <Skill skill="JavaScript" emoji="🙋" />
+      <Skill skill="MERN" emoji="4️⃣" />
+      <Skill skill="HTML+CSS" emoji="©️" />
+    </div>
+  );
+}
+function Skill(props) {
+  return (
+    <div className="skill">
+      <span>
+        {props.skill}
+        {props.emoji}
+      </span>
+    </div>
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Card />
   </React.StrictMode>
 );
 
